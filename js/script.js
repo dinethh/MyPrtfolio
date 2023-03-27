bdecd1083f4d2a154044dbe0870371bbca58b76d
dineth panditha <92966321+dinethh@users.noreply.github.com>
2023-03-27 22:37:56 +0530

    Navigation Bar is Compleated

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-$(document).ready(function(){
-//  Text typer
-var typed = new Typed(".auto-type", {
-    strings: ["Web Developer", "Mobile App Developer", "UI/UX Designer"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
-});
-
-// Project Counter
-jQuery(document).ready(function ($) {
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
+$(document).ready(function () {
+    //  Text typer
+    var typed = new Typed(".auto-type", {
+        strings: ["Web Developer", "Mobile App Developer", "UI/UX Designer"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
     });
-}); 
+
+    // // Project Counter
+    jQuery(document).ready(function ($) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    })
+
 
 });
