ca2b6c4bc9f45d2f74fe8a506f59bfae3f380c0f
dineth panditha <92966321+dinethh@users.noreply.github.com>
2023-04-28 15:40:31 +0530

    Case 5 and 6 Compleated

--- js/script.js
+++ js/script.js
@@ -15,5 +15,16 @@ $(document).ready(function () {
         });
     })
 
-
+ //Active Menu
+    const a = document.querySelectorAll(".links");
+    const sec = document.querySelectorAll("section");
+    function activeMenu() {
+        let len = sec.length;
+        while (--len && window.scrollY + 2.5 < sec[len].offsetTop) { }
+        a.forEach(ltx => ltx.classList.remove("active"));
+        a[len].classList.add("active");
+    }
+    activeMenu();
+    window.addEventListener("scroll", activeMenu);
+   
 });
