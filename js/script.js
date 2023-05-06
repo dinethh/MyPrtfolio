8326f54e3e5120161547c55a18c5cbdb25787400
dineth panditha <92966321+dinethh@users.noreply.github.com>
2023-05-06 11:42:22 +0530

    Gallery Functions are Compleated

--- js/script.js
+++ js/script.js
@@ -26,5 +26,26 @@ $(document).ready(function () {
     }
     activeMenu();
     window.addEventListener("scroll", activeMenu);
-   
+    
+
+     // Gallery Slider
+    let scrollContainer=document.querySelector(".gallery");
+    let backbtn=document.getElementById("back-btn");
+    let forwardbtn=document.getElementById("forward-btn");
+
+    scrollContainer.addEventListener("wheel",(evt)=>{
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+        scrollContainer.style.scrollBehavior ="auto";
+    });
+    
+    backbtn.addEventListener("click",()=>{
+        scrollContainer.style.scrollBehavior ="smooth";
+        scrollContainer.scrollLeft +=900;
+    });
+    forwardbtn.addEventListener("click",()=>{
+        scrollContainer.style.scrollBehavior ="smooth";
+        scrollContainer.scrollLeft -=900;
+    });
+
 });
