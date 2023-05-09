04f1d742d66787db9d25119534c536288962bcbe
dineth panditha <92966321+dinethh@users.noreply.github.com>
2023-05-09 22:06:15 +0530

    Sections are Updated

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
     //  Text typer
-    var typed = new Typed(".auto-type", {
-        strings: ["Web Developer", "Mobile App Developer", "UI/UX Designer"],
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
+    // var typed = new Typed(".auto-type", {
+    //     strings: ["Web Developer", "Mobile App Developer", "UI/UX Designer"],
+    //     typeSpeed: 50,
+    //     backSpeed: 50,
+    //     loop: true
+    // });
 
     // // Project Counter
-    jQuery(document).ready(function ($) {
-        $('.counter').counterUp({
-            delay: 10,
-            time: 1000
-        });
-    })
+    // jQuery(document).ready(function ($) {
+    //     $('.counter').counterUp({
+    //         delay: 10,
+    //         time: 1000
+    //     });
+    // })
 
  //Active Menu
-    const a = document.querySelectorAll(".links");
-    const sec = document.querySelectorAll("section");
-    function activeMenu() {
-        let len = sec.length;
-        while (--len && window.scrollY + 2.5 < sec[len].offsetTop) { }
-        a.forEach(ltx => ltx.classList.remove("active"));
-        a[len].classList.add("active");
-    }
-    activeMenu();
-    window.addEventListener("scroll", activeMenu);
+    // const a = document.querySelectorAll(".links");
+    // const sec = document.querySelectorAll("section");
+    // function activeMenu() {
+    //     let len = sec.length;
+    //     while (--len && window.scrollY + 2.5 < sec[len].offsetTop) { }
+    //     a.forEach(ltx => ltx.classList.remove("active"));
+    //     a[len].classList.add("active");
+    // }
+    // activeMenu();
+    // window.addEventListener("scroll", activeMenu);
     
 
      // Gallery Slider
@@ -33,11 +33,11 @@ $(document).ready(function () {
     let backbtn=document.getElementById("back-btn");
     let forwardbtn=document.getElementById("forward-btn");
 
-    scrollContainer.addEventListener("wheel",(evt)=>{
-        evt.preventDefault();
-        scrollContainer.scrollLeft += evt.deltaY;
-        scrollContainer.style.scrollBehavior ="auto";
-    });
+    // scrollContainer.addEventListener("wheel",(evt)=>{
+    //     evt.preventDefault();
+    //     scrollContainer.scrollLeft += evt.deltaY;
+    //     scrollContainer.style.scrollBehavior ="auto";
+    // });
     
     backbtn.addEventListener("click",()=>{
         scrollContainer.style.scrollBehavior ="smooth";
@@ -45,7 +45,7 @@ $(document).ready(function () {
     });
     forwardbtn.addEventListener("click",()=>{
         scrollContainer.style.scrollBehavior ="smooth";
-        scrollContainer.scrollLeft -=900;
+        scrollContainer.scrollLeft -=1000;
     });
 
 });
