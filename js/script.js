8166e2ffabc8da9a3b797900a03db9c8d7be6f03
dineth panditha <92966321+dinethh@users.noreply.github.com>
2023-05-15 11:36:04 +0530

    Structure is Updated

--- js/script.js
+++ js/script.js
@@ -1,34 +1,6 @@
 $(document).ready(function () {
-    //  Text typer
-    // var typed = new Typed(".auto-type", {
-    //     strings: ["Web Developer", "Mobile App Developer", "UI/UX Designer"],
-    //     typeSpeed: 50,
-    //     backSpeed: 50,
-    //     loop: true
-    // });
-
-    // // Project Counter
-    // jQuery(document).ready(function ($) {
-    //     $('.counter').counterUp({
-    //         delay: 10,
-    //         time: 1000
-    //     });
-    // })
-
- //Active Menu
-    // const a = document.querySelectorAll(".links");
-    // const sec = document.querySelectorAll("section");
-    // function activeMenu() {
-    //     let len = sec.length;
-    //     while (--len && window.scrollY + 2.5 < sec[len].offsetTop) { }
-    //     a.forEach(ltx => ltx.classList.remove("active"));
-    //     a[len].classList.add("active");
-    // }
-    // activeMenu();
-    // window.addEventListener("scroll", activeMenu);
-    
-
-     // Gallery Slider
+ 
+    // Gallery Slider
     let scrollContainer=document.querySelector(".gallery");
     let backbtn=document.getElementById("back-btn");
     let forwardbtn=document.getElementById("forward-btn");
@@ -49,21 +21,6 @@ $(document).ready(function () {
     });
 
 
-    //scrool up
-           // =================== CHANGE BACKGROUND HEADER ==================================
-function scrollHeader(){
-    const header = document.getElementById('header');
-    //when the scroll is greater than 80 viewpoint height, add the scroll-header class to header tag
-    if(this.scrollY >= 80 ) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
-}
-window.addEventListener('scroll',scrollHeader);
-// =================== SHOW SCROLL UP ==================================
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    //when the scroll is greater than 350 viewpoint height, add the show-scroll class to scroll-top class
-    if(this.scrollY >= 350 ) scrollUp.classList.add('show-scroll');
-    else scrollUp.classList.remove('show-scroll');
-} 
+  
 
 });
