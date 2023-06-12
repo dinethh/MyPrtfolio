d3f06aae128876c8e0e6488d12854c85079aaf2e
dineth panditha <92966321+dinethh@users.noreply.github.com>
2023-06-12 17:54:09 +0530

    Calculater is Compleated

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 
 $(document).ready(function () {
- 
     // Gallery Slider
-    let scrollContainer=document.querySelector(".gallery");
-    let backbtn=document.getElementById("back-btn");
-    let forwardbtn=document.getElementById("forward-btn");
+    let scrollContainer = document.querySelector(".gallery");
+    let backbtn = document.getElementById("back-btn");
+    let forwardbtn = document.getElementById("forward-btn");
 
-    // scrollContainer.addEventListener("wheel",(evt)=>{
-    //     evt.preventDefault();
-    //     scrollContainer.scrollLeft += evt.deltaY;
-    //     scrollContainer.style.scrollBehavior ="auto";
-    // });
-    
-    backbtn.addEventListener("click",()=>{
-        scrollContainer.style.scrollBehavior ="smooth";
-        scrollContainer.scrollLeft +=900;
+    backbtn.addEventListener("click", () => {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft += 900;
     });
-    forwardbtn.addEventListener("click",()=>{
-        scrollContainer.style.scrollBehavior ="smooth";
-        scrollContainer.scrollLeft -=1000;
+    forwardbtn.addEventListener("click", () => {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft -= 1000;
     });
+}
+);
 
 
-  
-
+ //Toggle menu
+$(document).ready(function () {
+    const hamber = document.querySelector(".hamber");
+    const navMenu = document.querySelector(".navMenu");
+    hamber.addEventListener("click", () => {
+        hamber.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    })
+    document.querySelector(".nav-link").forEach(n => n.addEventListener("click", () => {
+        hamber.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
 });
